f23060a996adcc30552b4e93c49804640e4c4c95
KmolYuan <pyslvs@gmail.com>
2021-03-02 11:38:38 +0800

    Bump version.

--- apimd/__init__.py
+++ apimd/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Yuan Chang"
 __copyright__ = "Copyright (C) 2020"
 __license__ = "MIT"
 __email__ = "pyslvs@gmail.com"
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 
 from .compiler import gen_api
 
